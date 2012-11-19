dcb83935ad0d9145489c72399118e56e11ddb9e3
Joakim Blomberg <joakim.blomberg@gmail.com>
2012-11-19 10:31:25 +0100

    Until I can debug culling just skip it

--- src/moaicore/MOAIPartitionCell.cpp
+++ src/moaicore/MOAIPartitionCell.cpp
@@ -118,9 +118,9 @@ void MOAIPartitionCell::GatherProps ( MOAIPartitionResultBuffer& results, const 
 		if ( prop == ignore ) continue;
 		
 		if (( mask == 0 ) || ( prop->mMask & mask )) {	
-			if ( !frustum.Cull ( prop->mBounds )) {
+			//if ( !frustum.Cull ( prop->mBounds )) {
 				prop->AddToSortBuffer ( results );
-			}
+			//}
 		}
 	}
 }
