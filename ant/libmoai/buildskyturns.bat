d851c056e6defe0025e8d3ddc11c5913a772e51a
Joakim Tall <joakim@fastmail.se>
2014-04-24 10:09:35 +0200

    no lua jit for android..

--- ant/libmoai/buildskyturns.bat
+++ ant/libmoai/buildskyturns.bat
@@ -1,1 +1,1 @@
-build.bat --use-luajit true --disable-adcolony --disable-billing --disable-chartboost --disable-facebook --disable-push --disable-tapjoy --disable-twitter --disable-playservices --release
+build.bat --use-luajit false --disable-adcolony --disable-billing --disable-chartboost --disable-facebook --disable-push --disable-tapjoy --disable-twitter --disable-playservices --release
