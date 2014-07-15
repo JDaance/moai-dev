3a353b978feaf4713ca5e209c71687a1d85d36b6
Joakim Tall <joakim@fastmail.se>
2014-07-15 18:56:07 +0200

    noise on android build finish

--- ant/libmoai/buildskyturns.bat
+++ ant/libmoai/buildskyturns.bat
@@ -1,1 +1,2 @@
 build.bat --use-luajit false --disable-adcolony --disable-billing --disable-chartboost --disable-facebook --disable-push --disable-tapjoy --disable-twitter --disable-playservices --release
+echo 
